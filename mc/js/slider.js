3a79871da977f331a18fb3b40fc8b20b532de351
mananshah141 <shahmanan10592@gmail.com>
2016-02-19 12:08:43 -0800

    Music Project

---
+++ mc/js/slider.js
@@ -0,0 +1,18 @@
+
+// XS top song more-link
+$( function(){
+
+	$(".more-link").click(function(){
+		$(".top-links-bar").addClass("open");
+
+	});
+});
+
+// XS top menu-link
+$( function(){
+
+	$(".menu").click(function(){
+		$(".header-menu").toggleClass("open");
+
+	});
+});
